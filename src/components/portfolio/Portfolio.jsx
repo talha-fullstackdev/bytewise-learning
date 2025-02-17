51343ae68fffdf9621be0be34e7af94ba4f1a506
talha-fullstackdev <talhafullstack786@gmail.com>
2025-02-17 14:50:45 -0800

    added initial portfolio component and made it responsive for mob and laptop

---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Portfolio = () => {
+  return (
+    <div className='mt-5 md:mt-0 text-center  px-2'>
+        <h2 className='text-black font-semibold text-lg md:text-3xl '>Portfolio</h2>
+        <hr className="w-[50px] h-[3px] bg-[#078791] border-none rounded-full mx-auto mt-3 mb-3 md:mt-4 md:mb-4" />
+
+        <p className='mt-2 md:text-md md:text-lg text-slate-800'>Explore our diverse portfolio showcasing our achievements in education, technology, and innovation.</p>
+    </div>
+  )
+}
+
+export default Portfolio
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Header from '../components/header/Header'
 import Hero from "../components/hero/Hero.jsx"
+import Portfolio from '../components/portfolio/Portfolio.jsx'
 const HomePage = () => {
   return (
     <>
     <Header/>
     <Hero/>
-
+    <Portfolio/>
     </>
   )
 }
