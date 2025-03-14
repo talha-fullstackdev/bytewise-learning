c98bba7ad2b74f90389cd9ceb8c253a99328fe89
talha-fullstackdev <talhafullstack786@gmail.com>
2025-03-14 15:28:55 -0800

    updated

--- src/components/hero/HeroPara.jsx
+++ src/components/hero/HeroPara.jsx
@@ -4,7 +4,7 @@ const HeroPara = () => {
     <div className='text-center px-2 text-md md:max-w-[600px] md:text-start md:font-semibold md:pl-10 font-semibold'>
         <h2 className='md:text-5xl text-xl'>Welcome to Bytewise Learning</h2>
         <p className='text-slate-300 md:mt-8 mt-2'>Empowering learners through accessible online education. Explore our diverse courses and resources today.</p>
-        <button className='md:mt-8 px-2 py-1 mt-4 rounded-xl max-w-[200px] border md:px-9 md:py-3 md:text-lg md:rounded-full '>Get started</button>
+        <button className='md:mt-8 px-2 py-1 mt-4 rounded-xl max-w-[200px] border md:px-9 md:py-3 md:text-lg md:rounded-full hover:bg-teal-600'>Get started</button>
     </div>
   )
 }
